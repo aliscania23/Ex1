6a081777e9ff3a1f22846f3f169ecd1d3fa347e6
ali.scania.s <115915790+aliscania23@users.noreply.github.com>
2022-11-24 09:03:59 +0330

    Create Ex1_5.py

---
+++ Ex1_5.py
@@ -0,0 +1,11 @@
+def EX1_5(a):
+    ''' (Float) => str
+    '''
+    if a<=1:
+        return infant
+    elif a<13:
+        return child 
+    elif a<20:
+        return teenager
+    else:
+        return adult
