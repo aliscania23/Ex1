2235b24884f662723d202a84a184ef4e9ce2b084
ali.scania.s <115915790+aliscania23@users.noreply.github.com>
2022-11-24 08:58:30 +0330

    Create Ex1_4.py

---
+++ Ex1_4.py
@@ -0,0 +1,17 @@
+def EX1_4(a):
+    if a==1:
+        return Monday
+    elif a==2:
+        return Tuesday
+    elif a==3:
+        return wednesday
+    elif a==4:
+        return Thursday
+    elif a==5:
+        return Friday
+    elif a==6:
+        return Saturday
+    elif a==7:
+        return Sunday
+    else:
+        return Error
